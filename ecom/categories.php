b1e8d9d8a673b960b77db58f4cd2b59b0321a9e5
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-21 18:19:33 +0500

    same as down

--- ecom/categories.php
+++ ecom/categories.php
@@ -17,44 +17,44 @@ if (isset($_GET['id']) && $_GET['id'] != '') {
                     <?php
                     foreach ($get_product as $list) {
                     ?>
-                    <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                        <div class="product-item bg-light mb-4">
-                            <div class="product-img position-relative overflow-hidden">
-                                <a href="product.php?id=<?php echo $list['id']?>"><img class="img-fluid w-100" src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt=""></a>
-                            </div>
-                            <div class="text-center py-4">
-                                <a class="h6 text-decoration-none text-truncate" href="product.php?id=<?php echo $list['id']?>"><?php echo $list['name']?></a>
-                                <div class="d-flex align-items-center justify-content-center mt-2">
-                                    <h5><?php echo $list['price']?></h5>
-                                    <h6 class="text-muted ml-2"><del><?php echo $list['mrp']?></del></h6>
+                        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                            <div class="product-item bg-light mb-4">
+                                <div class="product-img position-relative overflow-hidden">
+                                    <a href="product.php?id=<?php echo $list['id'] ?>"><img class="img-fluid w-100" src="<?php echo PRODUCT_IMAGE_SITE_PATH . $list['image'] ?>" alt=""></a>
                                 </div>
-                                <div class="d-flex align-items-center justify-content-center mb-1">
-                                    <small class="fa fa-star text-primary mr-1"></small>
-                                    <small class="fa fa-star text-primary mr-1"></small>
-                                    <small class="fa fa-star text-primary mr-1"></small>
-                                    <small class="fa fa-star text-primary mr-1"></small>
-                                    <small class="fa fa-star text-primary mr-1"></small>
-                                    <small>(<?php echo $list['price'] ?>)</small>
+                                <div class="text-center py-4">
+                                    <a class="h6 text-decoration-none text-truncate" href="product.php?id=<?php echo $list['id'] ?>"><?php echo $list['name'] ?></a>
+                                    <div class="d-flex align-items-center justify-content-center mt-2">
+                                        <h5><?php echo $list['price'] ?></h5>
+                                        <h6 class="text-muted ml-2"><del><?php echo $list['mrp'] ?></del></h6>
+                                    </div>
+                                    <div class="d-flex align-items-center justify-content-center mb-1">
+                                        <small class="fa fa-star text-primary mr-1"></small>
+                                        <small class="fa fa-star text-primary mr-1"></small>
+                                        <small class="fa fa-star text-primary mr-1"></small>
+                                        <small class="fa fa-star text-primary mr-1"></small>
+                                        <small class="fa fa-star text-primary mr-1"></small>
+                                        <small>(<?php echo $list['price'] ?>)</small>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     <?php
                     }
                     ?>
                 </div>
             </div>
-<?php
+        <?php
         }
     } else {
-?>
+        ?>
         <script>
             window.location.href = 'index.php';
         </script>
-<?php
+    <?php
     }
 } else {
-?>
+    ?>
     <script>
         window.location.href = 'index.php';
     </script>
