6b1a29be6e54c8e45eb12178aca81bfa69fcf03b
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-21 13:17:25 +0500

    product php full sucs

--- ecom/index.php
+++ ecom/index.php
@@ -267,15 +267,19 @@
     <div class="container-fluid pt-5 pb-3">
         <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Featured Products</span></h2>
         <div class="row px-xl-5">
+        <?php
+							$get_product=get_product($con,4);
+							foreach($get_product as $list){
+							?>
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <a href="#"><img class="img-fluid w-100" src="img/product-1.jpg" alt=""></a>
+                        <a href="#"><img class="img-fluid w-100" src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt=""></a>
                     </div>
                     <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Product Name Goes Here</a>
+                        <a class="h6 text-decoration-none text-truncate" href="#"><?php echo $list['name']?></a>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
+                            <h5>Rs.<?php echo $list['price']?></h5><h6 class="text-muted ml-2"><del>Rs.<?php echo $list['mrp']?></del></h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
@@ -283,11 +287,12 @@
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
+                            <small>(<?php echo $list['price']?>)</small>
                         </div>
                     </div>
                 </div>
             </div>
+            <?php } ?>
         </div>
     </div>
     <!-- Products End -->
