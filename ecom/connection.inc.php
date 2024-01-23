a1accb3547ff162802672e008a6e15a7cc16bfd2
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-23 17:02:27 +0500

    confg conections files both

--- ecom/connection.inc.php
+++ ecom/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","ecom");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
-define('SITE_PATH','http://127.0.0.1/php/ecom/');
+$con=mysqli_connect("localhost","root","","shop4");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/shop/ecom/');
+define('SITE_PATH','http://127.0.0.1/shop/ecom/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
