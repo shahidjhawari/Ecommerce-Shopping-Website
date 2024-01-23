ebbd161180c092083019791d51e4dc97c047f98e
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-23 17:34:43 +0500

    only add php in checkout

--- ecom/checkout.php
+++ ecom/checkout.php
@@ -1,4 +1,57 @@
-<?php require('top.php'); ?>
+<?php 
+require('top.php');
+if(!isset($_SESSION['cart']) || count($_SESSION['cart'])==0){
+	?>
+	<script>
+		window.location.href='index.php';
+	</script>
+	<?php
+}
+
+$cart_total=0;
+
+if(isset($_POST['submit'])){
+	$address=get_safe_value($con,$_POST['address']);
+	$city=get_safe_value($con,$_POST['city']);
+	$pincode=get_safe_value($con,$_POST['pincode']);
+	$payment_type=get_safe_value($con,$_POST['payment_type']);
+	$user_id=$_SESSION['USER_ID'];
+	foreach($_SESSION['cart'] as $key=>$val){
+		$productArr=get_product($con,'','',$key);
+		$price=$productArr[0]['price'];
+		$qty=$val['qty'];
+		$cart_total=$cart_total+($price*$qty);
+		
+	}
+	$total_price=$cart_total;
+	$payment_status='pending';
+	if($payment_type=='cod'){
+		$payment_status='success';
+	}
+	$order_status='pending';
+	$added_on=date('Y-m-d h:i:s');
+	
+	
+	mysqli_query($con,"insert into `order`(user_id,address,city,pincode,payment_type,payment_status,order_status,added_on,total_price) values('$user_id','$address','$city','$pincode','$payment_type','$payment_status','$order_status','$added_on','$total_price')");
+	
+	$order_id=mysqli_insert_id($con);
+	
+	foreach($_SESSION['cart'] as $key=>$val){
+		$productArr=get_product($con,'','',$key);
+		$price=$productArr[0]['price'];
+		$qty=$val['qty'];
+		
+		mysqli_query($con,"insert into `order_detail`(order_id,product_id,qty,price) values('$order_id','$key','$qty','$price')");
+	}
+	
+	unset($_SESSION['cart'])
+	?>
+	<script>
+		window.location.href='thank_you.php';
+	</script>
+	<?php
+}
+?>
 
 
 
