3fad3dd88692646ac86b72fd5f13c27ab647c26f
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-23 17:57:31 +0500

    payment section pending

--- ecom/checkout.php
+++ ecom/checkout.php
@@ -145,41 +145,44 @@ if(isset($_POST['submit'])){
                 </div>
             </div>
             <div class="col-lg-4">
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Order
-                        Total</span></h5>
-                <div class="bg-light p-30 mb-5">
-                    <div class="border-bottom">
-                        <h6 class="mb-3">Products</h6>
-                        <div class="d-flex justify-content-between">
-                            <p>Product Name 1</p>
-                            <p>$150</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p>Product Name 2</p>
-                            <p>$150</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p>Product Name 3</p>
-                            <p>$150</p>
-                        </div>
-                    </div>
-                    <div class="border-bottom pt-3 pb-2">
-                        <div class="d-flex justify-content-between mb-3">
-                            <h6>Subtotal</h6>
-                            <h6>$150</h6>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <h6 class="font-weight-medium">Shipping</h6>
-                            <h6 class="font-weight-medium">$10</h6>
-                        </div>
-                    </div>
-                    <div class="pt-2">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5>Total</h5>
-                            <h5>$160</h5>
-                        </div>
-                    </div>
+    <h5 class="section-title position-relative text-uppercase mb-3">
+        <span class="bg-secondary pr-3">Order Total</span>
+    </h5>
+    <div class="bg-light p-30 mb-5">
+        <div class="border-bottom">
+            <h6 class="mb-3">Products</h6>
+            <?php
+            $cart_total = 0;
+            foreach ($_SESSION['cart'] as $key => $val) {
+                $productArr = get_product($con, '', '', $key);
+                $pname = $productArr[0]['name'];
+                $mrp = $productArr[0]['mrp'];
+                $price = $productArr[0]['price'];
+                $image = $productArr[0]['image'];
+                $qty = $val['qty'];
+                $cart_total = $cart_total + ($price * $qty);
+                ?>
+                <div class="d-flex justify-content-between">
+                    <p><?php echo $pname ?></p>
+                    <p>Rs.<?php echo $price ?></p>
                 </div>
+            <?php } ?>
+        </div>
+        <div class="border-bottom pt-3 pb-2">
+            <div class="d-flex justify-content-between mb-3">
+                <h6>Shiping Fee</h6>
+                <h6>Rs.30</h6>
+            </div>
+        </div>
+        <div class="pt-2">
+            <div class="d-flex justify-content-between mt-2">
+                <h5>Total</h5>
+                <h5>Rs.<?php echo $cart_total + 30 ?></h5>
+            </div>
+        </div>
+    </div>
+</div>
+
                 <div class="mb-5">
                     <h5 class="section-title position-relative text-uppercase mb-3"><span
                             class="bg-secondary pr-3">Payment</span></h5>
