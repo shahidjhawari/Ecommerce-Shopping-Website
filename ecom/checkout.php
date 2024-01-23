fbd3b2670bd3c3e007c05d733caa81f0cb8e168b
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-23 18:23:26 +0500

    checkout done - all ecomm4 sucess done

--- ecom/checkout.php
+++ ecom/checkout.php
@@ -69,147 +69,147 @@ if (isset($_POST['submit'])) {
 
 <!-- Checkout Start -->
 <div class="container-fluid">
-<?php 
-	if(isset($_SESSION['USER_LOGIN'])){
-	?>
-    <div class="row px-xl-5">
-        <div class="col-lg-8">
-            <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Billing
-                    Address</span></h5>
-            <div class="bg-light p-30 mb-5">
-                <form id="login-form" method="post">
-                <div class="row">
-                    <div class="col-md-6 form-group">
-                        <label>Full Name</label>
-                        <input class="form-control" type="text" name="address" placeholder="Full Name" required>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label>Address</label>
-                        <input class="form-control" type="text" name="city" placeholder="Address" required>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label>Phone Number</label>
-                        <input class="form-control" type="text" name="pincode" placeholder="Phone Number" required>
+    <?php
+    if (isset($_SESSION['USER_LOGIN'])) {
+    ?>
+        <div class="row px-xl-5">
+            <div class="col-lg-8">
+                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Billing
+                        Address</span></h5>
+                <div class="bg-light p-30 mb-5">
+                    <form id="login-form" method="post">
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label>Full Name</label>
+                                <input class="form-control" type="text" name="address" placeholder="Full Name" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Address</label>
+                                <input class="form-control" type="text" name="city" placeholder="Address" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Phone Number</label>
+                                <input class="form-control" type="text" name="pincode" placeholder="Phone Number" required>
+                            </div>
+                        </div>
+                </div>
+                <div class="collapse mb-5" id="shipping-address">
+                    <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Shipping Address</span></h5>
+                    <div class="bg-light p-30">
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label>First Name</label>
+                                <input class="form-control" type="text" placeholder="John">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Last Name</label>
+                                <input class="form-control" type="text" placeholder="Doe">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>E-mail</label>
+                                <input class="form-control" type="text" placeholder="example@email.com">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Mobile No</label>
+                                <input class="form-control" type="text" placeholder="+123 456 789">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Address Line 1</label>
+                                <input class="form-control" type="text" placeholder="123 Street">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Address Line 2</label>
+                                <input class="form-control" type="text" placeholder="123 Street">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Country</label>
+                                <select class="custom-select">
+                                    <option selected>United States</option>
+                                    <option>Afghanistan</option>
+                                    <option>Albania</option>
+                                    <option>Algeria</option>
+                                </select>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>City</label>
+                                <input class="form-control" type="text" placeholder="New York">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>State</label>
+                                <input class="form-control" type="text" placeholder="New York">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>ZIP Code</label>
+                                <input class="form-control" type="text" placeholder="123">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="collapse mb-5" id="shipping-address">
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Shipping Address</span></h5>
-                <div class="bg-light p-30">
-                    <div class="row">
-                        <div class="col-md-6 form-group">
-                            <label>First Name</label>
-                            <input class="form-control" type="text" placeholder="John">
+            <div class="col-lg-4">
+                <h5 class="section-title position-relative text-uppercase mb-3">
+                    <span class="bg-secondary pr-3">Order Total</span>
+                </h5>
+                <div class="bg-light p-30 mb-5">
+                    <div class="border-bottom">
+                        <h6 class="mb-3">Products</h6>
+                        <?php
+                        $cart_total = 0;
+                        foreach ($_SESSION['cart'] as $key => $val) {
+                            $productArr = get_product($con, '', '', $key);
+                            $pname = $productArr[0]['name'];
+                            $mrp = $productArr[0]['mrp'];
+                            $price = $productArr[0]['price'];
+                            $image = $productArr[0]['image'];
+                            $qty = $val['qty'];
+                            $cart_total = $cart_total + ($price * $qty);
+                        ?>
+                            <div class="d-flex justify-content-between">
+                                <p><?php echo $pname ?></p>
+                                <p>Rs.<?php echo $price ?></p>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <div class="border-bottom pt-3 pb-2">
+                        <div class="d-flex justify-content-between mb-3">
+                            <h6>Shiping Fee</h6>
+                            <h6>Rs.30</h6>
                         </div>
-                        <div class="col-md-6 form-group">
-                            <label>Last Name</label>
-                            <input class="form-control" type="text" placeholder="Doe">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>E-mail</label>
-                            <input class="form-control" type="text" placeholder="example@email.com">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Mobile No</label>
-                            <input class="form-control" type="text" placeholder="+123 456 789">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Address Line 1</label>
-                            <input class="form-control" type="text" placeholder="123 Street">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Address Line 2</label>
-                            <input class="form-control" type="text" placeholder="123 Street">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Country</label>
-                            <select class="custom-select">
-                                <option selected>United States</option>
-                                <option>Afghanistan</option>
-                                <option>Albania</option>
-                                <option>Algeria</option>
-                            </select>
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>City</label>
-                            <input class="form-control" type="text" placeholder="New York">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>State</label>
-                            <input class="form-control" type="text" placeholder="New York">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>ZIP Code</label>
-                            <input class="form-control" type="text" placeholder="123">
+                    </div>
+                    <div class="pt-2">
+                        <div class="d-flex justify-content-between mt-2">
+                            <h5>Total</h5>
+                            <h5>Rs.<?php echo $cart_total + 30 ?></h5>
                         </div>
                     </div>
                 </div>
+                <div class="mb-5">
+                    <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Payment</span></h5>
+                    <div class="bg-light p-30">
+                        <div class="form-group">
+                            <div class="custom-control custom-radio">
+                                <input type="radio" name="payment_type" class="custom-control-input" id="cod" required>
+                                <label class="custom-control-label" for="cod">Cash On Delivery</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-radio">
+                                <input type="radio" class="custom-control-input" name="payment" id="directcheck" disabled>
+                                <label class="custom-control-label" for="directcheck">JazzCash</label>
+                            </div>
+                        </div>
+                        <div class="form-group mb-4">
+                            <div class="custom-control custom-radio">
+                                <input type="radio" class="custom-control-input" name="payment" id="banktransfer" disabled>
+                                <label class="custom-control-label" for="banktransfer">EasyPaisa</label>
+                            </div>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-block btn-primary font-weight-bold py-3">Place Order</button>
+                    </div>
+                </div>
+                </form>
             </div>
         </div>
-        <div class="col-lg-4">
-            <h5 class="section-title position-relative text-uppercase mb-3">
-                <span class="bg-secondary pr-3">Order Total</span>
-            </h5>
-            <div class="bg-light p-30 mb-5">
-                <div class="border-bottom">
-                    <h6 class="mb-3">Products</h6>
-                    <?php
-                    $cart_total = 0;
-                    foreach ($_SESSION['cart'] as $key => $val) {
-                        $productArr = get_product($con, '', '', $key);
-                        $pname = $productArr[0]['name'];
-                        $mrp = $productArr[0]['mrp'];
-                        $price = $productArr[0]['price'];
-                        $image = $productArr[0]['image'];
-                        $qty = $val['qty'];
-                        $cart_total = $cart_total + ($price * $qty);
-                    ?>
-                        <div class="d-flex justify-content-between">
-                            <p><?php echo $pname ?></p>
-                            <p>Rs.<?php echo $price ?></p>
-                        </div>
-                    <?php } ?>
-                </div>
-                <div class="border-bottom pt-3 pb-2">
-                    <div class="d-flex justify-content-between mb-3">
-                        <h6>Shiping Fee</h6>
-                        <h6>Rs.30</h6>
-                    </div>
-                </div>
-                <div class="pt-2">
-                    <div class="d-flex justify-content-between mt-2">
-                        <h5>Total</h5>
-                        <h5>Rs.<?php echo $cart_total + 30 ?></h5>
-                    </div>
-                </div>
-            </div>
-            <div class="mb-5">
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Payment</span></h5>
-                <div class="bg-light p-30">
-                    <div class="form-group">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" name="payment_type" class="custom-control-input" id="cod" required>
-                            <label class="custom-control-label" for="cod">Cash On Delivery</label>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" class="custom-control-input" name="payment" id="directcheck" disabled>
-                            <label class="custom-control-label" for="directcheck">JazzCash</label>
-                        </div>
-                    </div>
-                    <div class="form-group mb-4">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" class="custom-control-input" name="payment" id="banktransfer" disabled>
-                            <label class="custom-control-label" for="banktransfer">EasyPaisa</label>
-                        </div>
-                    </div>
-                    <button type="submit" name="submit" class="btn btn-block btn-primary font-weight-bold py-3">Place Order</button>
-                </div>
-            </div>
-            </form>
-        </div>
-    </div>
     <?php } else {
         echo '<div class="container"><div class="alert alert-danger">Please login to view your orders.</div></div>';
     } ?>
