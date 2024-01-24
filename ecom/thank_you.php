bcaf88a41dfb762561bf142f6ed8310298fe0d92
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-24 12:45:57 +0500

    same as down

--- ecom/thank_you.php
+++ ecom/thank_you.php
@@ -1,4 +1,13 @@
-<?php require('top.php'); ?>
+<?php 
+require('top.php');
+if(!isset($_SESSION['USER_LOGIN'])){
+	?>
+	<script>
+	window.location.href='index.php';
+	</script>
+	<?php
+}
+?>
 
 <div class="container mt-5">
     <div class="row justify-content-center">
