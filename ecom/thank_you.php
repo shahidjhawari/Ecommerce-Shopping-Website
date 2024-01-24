b01e4fac090a5fd8955bc96f2a4dd70e52141a3b
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-24 12:39:51 +0500

    thank you page done all

--- ecom/thank_you.php
+++ ecom/thank_you.php
@@ -1,5 +1,15 @@
 <?php require('top.php'); ?>
 
-
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6 text-center">
+            <img src="img/order_placed.png" width="50px" alt="Order Placed Icon" class="img-fluid mb-4">
+            <h1 class="mb-3">Your order has been placed!</h1>
+            <p class="lead">Thank you for choosing our services. Your order is confirmed, and we are processing it.</p>
+            <p>We will send you an email with further details shortly.</p>
+            <a href="#" class="btn btn-primary mt-3"><b>Track Your Order</b></a>
+        </div>
+    </div>
+</div>
 
 <?php require('footer.php'); ?>
