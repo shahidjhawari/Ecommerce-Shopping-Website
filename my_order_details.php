54d7b4348359369de5628e976e1990738198523f
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-01-24 13:00:00 +0500

    adding only my order detail page & top php

---
+++ my_order_details.php
@@ -0,0 +1,15 @@
+<?php 
+require('top.php');
+if(!isset($_SESSION['USER_LOGIN'])){
+	?>
+	<script>
+	window.location.href='index.php';
+	</script>
+	<?php
+}
+$order_id=get_safe_value($con,$_GET['id']);
+?>
+
+
+
+<?php require('footer.php'); ?>   
