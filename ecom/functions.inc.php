c613e0240d1a5c1f639be638d6ad1180291d7d5a
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-02-12 10:55:38 +0500

    add ecomm 6all files

--- ecom/functions.inc.php
+++ ecom/functions.inc.php
@@ -17,7 +17,7 @@ function get_safe_value($con,$str){
 	}
 }
 
-function get_product($con,$limit='',$cat_id='',$product_id=''){
+function get_product($con,$limit='',$cat_id='',$product_id='',$search_str='',$sort_order=''){
 	$sql="select product.*,categories.categories from product,categories where product.status=1 ";
 	if($cat_id!=''){
 		$sql.=" and product.categories_id=$cat_id ";
@@ -26,11 +26,18 @@ function get_product($con,$limit='',$cat_id='',$product_id=''){
 		$sql.=" and product.id=$product_id ";
 	}
 	$sql.=" and product.categories_id=categories.id ";
-	$sql.=" order by product.id desc";
+	if($search_str!=''){
+		$sql.=" and (product.name like '%$search_str%' or product.description like '%$search_str%') ";
+	}
+	if($sort_order!=''){
+		$sql.=$sort_order;
+	}else{
+		$sql.=" order by product.id desc ";
+	}
 	if($limit!=''){
 		$sql.=" limit $limit";
 	}
-	
+	//echo $sql;
 	$res=mysqli_query($con,$sql);
 	$data=array();
 	while($row=mysqli_fetch_assoc($res)){
