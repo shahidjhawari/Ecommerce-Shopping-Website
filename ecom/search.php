c7755ba2a9a4b742dc5c4cc25fa92e3c137e3f47
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-02-12 11:00:59 +0500

    add search file

---
+++ ecom/search.php
@@ -0,0 +1,17 @@
+<?php 
+require('top.php');
+$str=mysqli_real_escape_string($con,$_GET['str']);
+if($str!=''){
+	$get_product=get_product($con,'','','',$str);
+}else{
+	?>
+	<script>
+	window.location.href='index.php';
+	</script>
+	<?php
+}										
+?>
+
+
+
+<?php require('footer.php')?>      
