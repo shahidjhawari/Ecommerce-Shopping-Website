5f1cc4230afe74a4102b3fe6644930f7dab8a636
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-02-14 12:19:46 +0500

    adding wishlist page & code

---
+++ ecom/wishlist.php
@@ -0,0 +1,17 @@
+<?php 
+require('top.php');
+if(!isset($_SESSION['USER_LOGIN'])){
+	?>
+	<script>
+	window.location.href='index.php';
+	</script>
+	<?php
+}
+$uid=$_SESSION['USER_ID'];
+
+$res=mysqli_query($con,"select product.name,product.image,product.price,product.mrp,wishlist.id from product,wishlist where wishlist.product_id=product.id and wishlist.user_id='$uid'");
+?>
+
+
+
+<?php require('footer.php')?>   
