66bab818190781d17128ee24842a7c10bebe1a87
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-18 21:19:35 +0500

    change connection

--- connection.inc.php
+++ connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","shop8");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/shop/ecom/');
-define('SITE_PATH','http://127.0.0.1/shop/ecom/');
+$con=mysqli_connect("localhost","root","","shop");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/shop/');
+define('SITE_PATH','http://127.0.0.1/shop/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
